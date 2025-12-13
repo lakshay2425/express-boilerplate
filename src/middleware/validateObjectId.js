96c796fda438d4c3c10ccefcba043273e441bb6f
Lakshay <lakshay12290@gmail.com>
2025-12-13 10:47:04 +0530

    feat: Add validateObjectId middleware

---
+++ src/middleware/validateObjectId.js
@@ -0,0 +1,21 @@
+import * as z from 'zod'
+import mongoose from 'mongoose'
+import createHttpError from 'http-errors'
+
+
+const objectIdSchema = z.string().refine((val) => mongoose.Types.ObjectId.isValid(val), {
+  message: 'Invalid ObjectId format',
+})
+
+export const validateObjectId = ({paramName, type}) => {
+    return async (req, res, next) => {
+        const ObjectId = req[type][paramName];
+        if(!ObjectId) return next(createHttpError(400, `Missing objectId`));
+        const result = objectIdSchema.safeParse(ObjectId);
+
+        if(!result.success) {
+            return res.status(400).json({error: `Invalid ObjectId`});
+        }
+        next();
+    } 
+}
