de4593035063e6b28f7ab0833e8e6bedae431967
Lakshay <lakshay12290@gmail.com>
2025-12-13 10:35:55 +0530

    feat: Add optionalAuthMiddleware, update .env.sample and config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,10 @@
 const _config = {
     PORT : process.env.PORT || 3000,
     NODE_ENVIRONMENT : process.env.NODE_ENV || 'development',
-    dbURI : process.env.DB_URI || 'mongodb://localhost:27017/base'
+    dbURI : process.env.DB_URI || 'mongodb://localhost:27017/base',
+    JWT_PUBLIC_KEY : process.env.JWT_PUBLIC_KEY,
+    BYPASS_AUTH : process.env.BYPASS_AUTH,
+    TEST_USER_EMAIL : process.env.TEST_USER_EMAIL
 }
 
 
